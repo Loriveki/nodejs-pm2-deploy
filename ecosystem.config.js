5fab43db20690c37e6248a698befabba54661bc8
Loriveki <loriveki@mail.ru>
2025-09-11 21:38:08 +0300

    Настройка PM2 и деплой

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,10 +12,11 @@ module.exports = {
   apps: [
     {
       name: 'mesto-backend',
-      script: 'dist/app.js',     
+      script: 'dist/app.js',
       cwd: './backend',
       env: { NODE_ENV: 'production' },
       interpreter: 'node',
+      autorestart: true,
     },
     {
       name: 'mesto-frontend',
@@ -23,6 +24,7 @@ module.exports = {
       args: 'serve -s build -l 3001',
       cwd: './frontend',
       env: { NODE_ENV: 'production' },
+      autorestart: true,
     },
   ],
 
@@ -34,21 +36,23 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       ssh_options: `IdentityFile=${DEPLOY_SSH_KEY}`,
+
       'pre-deploy': `
-        # создаём папки на сервере, если их нет
-        ssh ${DEPLOY_USER}@${DEPLOY_HOST} "mkdir -p ${DEPLOY_PATH}/backend ${DEPLOY_PATH}/frontend"
+        # Создать папки
+        ssh ${DEPLOY_USER}@${DEPLOY_HOST} "mkdir -p ${DEPLOY_PATH}/backend ${DEPLOY_PATH}/frontend" &&
+        # Копируем env-файлы
+        scp ./backend/.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend/.env
       `,
+
       'post-deploy': `
         # --- Бэкенд ---
         cd backend &&
-        git reset --hard &&  # чтобы сбросить локальные изменения
         npm install &&
         npm run build &&
         pm2 reload ecosystem.config.js --only mesto-backend --env production &&
 
         # --- Фронтенд ---
         cd ../frontend &&
-        git reset --hard &&
         npm install &&
         NODE_OPTIONS=--openssl-legacy-provider npm run build &&
         pm2 reload ecosystem.config.js --only mesto-frontend --env production
