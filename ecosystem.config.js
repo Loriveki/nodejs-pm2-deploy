64a922c3c135c81fb8af9eefe6f1cfb3c8c93b78
Loriveki <loriveki@mail.ru>
2025-09-11 19:28:17 +0300

    Настройка PM2 и деплой

---
+++ ecosystem.config.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_REPO,
+  DEPLOY_PATH,
+  DEPLOY_SSH_KEY,
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'mesto-backend',
+      script: 'dist/app.js', 
+      cwd: './backend',
+      env: { NODE_ENV: 'production' },
+      interpreter: 'node',
+    },
+    {
+      name: 'mesto-frontend',
+      script: 'npx',
+      args: 'serve -s build -l 3001',
+      cwd: './frontend',
+      env: { NODE_ENV: 'production' },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: 'origin/review',
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      ssh_options: `IdentityFile=${DEPLOY_SSH_KEY}`,
+      'post-deploy': `
+        scp ./backend/.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend/.env &&
+        cd backend &&
+        npm install &&
+        npm run build &&
+        cd ../frontend &&
+        npm install &&
+        NODE_OPTIONS=--openssl-legacy-provider npm run build &&
+        pm2 reload ecosystem.config.js --env production
+      `,
+    },
+  },
+};
