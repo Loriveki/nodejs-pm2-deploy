c3c4b6e45259e231e4f775f3199e9ec39b94e0ed
Loriveki <loriveki@mail.ru>
2025-09-11 23:37:28 +0300

    Настройка PM2 и деплой

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       name: 'mesto-backend',
       script: 'dist/app.js',
-      cwd: './backend',
+      cwd: `${DEPLOY_PATH}/backend`,
       env: { NODE_ENV: 'production' },
       interpreter: 'node',
       autorestart: true,
@@ -22,7 +22,7 @@ module.exports = {
       name: 'mesto-frontend',
       script: 'npx',
       args: 'serve -s build -l 3001',
-      cwd: './frontend',
+      cwd: `${DEPLOY_PATH}/frontend`,
       env: { NODE_ENV: 'production' },
       autorestart: true,
     },
@@ -32,30 +32,29 @@ module.exports = {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
-      ref: 'origin/review',
       repo: DEPLOY_REPO,
+      ref: 'origin/review',
       path: DEPLOY_PATH,
       ssh_options: `IdentityFile=${DEPLOY_SSH_KEY}`,
 
       'pre-deploy': `
-        # Создать папки
-        ssh ${DEPLOY_USER}@${DEPLOY_HOST} "mkdir -p ${DEPLOY_PATH}/backend ${DEPLOY_PATH}/frontend" &&
-        # Копируем env-файлы
-        scp ./backend/.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend/.env
+        ssh ${DEPLOY_USER}@${DEPLOY_HOST} "mkdir -p ${DEPLOY_PATH}" &&
+        ssh ${DEPLOY_USER}@${DEPLOY_HOST} "rm -rf ${DEPLOY_PATH}/backend ${DEPLOY_PATH}/frontend" &&
+        git clone -b review ${DEPLOY_REPO} ${DEPLOY_PATH}
       `,
 
       'post-deploy': `
         # --- Бэкенд ---
-        cd backend &&
+        cd ${DEPLOY_PATH}/backend &&
         npm install &&
         npm run build &&
-        pm2 reload ecosystem.config.js --only mesto-backend --env production &&
+        pm2 reload ${DEPLOY_PATH}/ecosystem.config.js --only mesto-backend --env production &&
 
         # --- Фронтенд ---
-        cd ../frontend &&
+        cd ${DEPLOY_PATH}/frontend &&
         npm install &&
         NODE_OPTIONS=--openssl-legacy-provider npm run build &&
-        pm2 reload ecosystem.config.js --only mesto-frontend --env production
+        pm2 reload ${DEPLOY_PATH}/ecosystem.config.js --only mesto-frontend --env production
       `,
     },
   },
