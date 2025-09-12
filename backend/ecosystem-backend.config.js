353b56ceaa516808490bbfb4319a038850a75bd0
Loriveki <loriveki@mail.ru>
2025-09-12 20:40:00 +0300

    Настройка PM2 и деплой

--- backend/ecosystem-backend.config.js
+++ backend/ecosystem-backend.config.js
@@ -31,11 +31,17 @@ module.exports = {
       ssh_options: DEPLOY_SSH_KEY ? `IdentityFile=${DEPLOY_SSH_KEY}` : '',
 
       'pre-setup': `
-        ssh ${DEPLOY_USER}@${DEPLOY_HOST} "mkdir -p ${DEPLOY_PATH}"
+        ssh ${DEPLOY_USER}@${DEPLOY_HOST} "mkdir -p ${DEPLOY_PATH}/current/backend"
       `,
 
       'post-deploy': `
         cd ${DEPLOY_PATH}/current/backend &&
+        if [ ! -d ".git" ]; then
+          git clone -b ${DEPLOY_REF} ${DEPLOY_REPO} .;
+        else
+          git reset --hard;
+          git pull origin ${DEPLOY_REF};
+        fi &&
         npm install &&
         npm run build &&
         pm2 startOrReload ${DEPLOY_PATH}/current/backend/ecosystem-backend.config.js --only mesto-backend --env production
