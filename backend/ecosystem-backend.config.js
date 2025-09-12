ebaeadb8448c0013a2935e39a1c2725a9c9577d1
Loriveki <loriveki@mail.ru>
2025-09-12 20:12:01 +0300

    Настройка PM2 и деплой

--- backend/ecosystem-backend.config.js
+++ backend/ecosystem-backend.config.js
@@ -25,16 +25,16 @@ module.exports = {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
-      ref: DEPLOY_REF,
+      ref: `origin/${DEPLOY_REF}`,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       ssh_options: DEPLOY_SSH_KEY ? `IdentityFile=${DEPLOY_SSH_KEY}` : '',
 
       'post-deploy': `
-      cd ${DEPLOY_PATH}/current/backend &&
-      npm install &&
-      npm run build &&
-      pm2 startOrReload ecosystem-backend.config.js --env production
+        cd ${DEPLOY_PATH}/current/backend &&
+        npm install &&
+        npm run build &&
+        pm2 startOrReload ecosystem-backend.config.js --env production
       `,
     },
   },
