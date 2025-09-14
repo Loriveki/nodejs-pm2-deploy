9d9b566d3d8fd819169b95e932de3d9830a707c3
Loriveki <loriveki@mail.ru>
2025-09-14 16:14:11 +0300

    Настройка PM2 и деплой

--- backend/ecosystem-backend.config.js
+++ backend/ecosystem-backend.config.js
@@ -23,21 +23,19 @@ module.exports = {
     },
   ],
 
- deploy: {
-  production: {
-    user: DEPLOY_USER,
-    host: DEPLOY_HOST,
-    ref: DEPLOY_REF,
-    repo: DEPLOY_REPO,
-    path: DEPLOY_PATH,
-    key: DEPLOY_SSH_KEY,
-    'pre-deploy': 'scp -i /home/loriveki/.ssh/new_key/private_key ./backend/.env user@158.160.185.102:/home/user/shared/.env',
-    'post-deploy': `
-      cd ${DEPLOY_PATH}/current &&
-      npm install &&
-      npm run build &&
-      pm2 reload backend/ecosystem-backend.config.js --env production
-    `,
-  },
+ production: {
+  user: DEPLOY_USER,
+  host: DEPLOY_HOST,
+  ref: DEPLOY_REF,
+  repo: DEPLOY_REPO,
+  path: DEPLOY_PATH,
+  key: '/home/loriveki/.ssh/new_key/private_key',
+  'pre-deploy': 'scp ./backend/.env user@158.160.185.102:/home/user/shared/.env',
+  'post-deploy': `
+    cd ${DEPLOY_PATH}/current &&
+    npm install &&
+    npm run build &&
+    pm2 reload backend/ecosystem-backend.config.js --env production
+  `,
 },
 };
