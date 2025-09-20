cdf2fec88d74c36678519f578c42cf662cd02880
Loriveki <loriveki@mail.ru>
2025-09-20 14:12:24 +0000

    Изменение ecosystem

--- backend/ecosystem-backend.config.js
+++ backend/ecosystem-backend.config.js
@@ -16,12 +16,12 @@ module.exports = {
   apps: [
     {
       name: 'mesto-backend',
-      script: '/home/user/current/backend/dist/app.js', 
+      script: '/home/user/current/backend/dist/app.js',
       cwd: '/home/user/current/backend',
       watch: false,
       autorestart: true,
       max_restarts: 10,
-      env: {
+      env_production: { 
         NODE_ENV: 'production',
         DOTENV_CONFIG_PATH: '/home/user/shared/.env',
       },
@@ -39,7 +39,9 @@ module.exports = {
       'pre-deploy-local': `bash ${path.resolve(__dirname, 'pre-deploy.sh')}`,
       'post-deploy': `
         echo "Starting post-deploy..." &&
+        echo "DEPLOY_PATH is ${DEPLOY_PATH}" &&
         cd ${DEPLOY_PATH}/current/backend || { echo "Failed to cd to ${DEPLOY_PATH}/current/backend"; exit 1; } &&
+        chmod +x post-deploy.sh || { echo "Failed to chmod post-deploy.sh"; exit 1; } &&
         bash post-deploy.sh || { echo "Failed to run post-deploy.sh"; exit 1; }
       `,
     },
