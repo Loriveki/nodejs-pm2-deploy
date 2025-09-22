ae38ef55f80759a4fa368fbf0938709f03d36c53
Loriveki <loriveki@mail.ru>
2025-09-22 17:21:40 +0300

    Изменение ecosystem

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,19 @@
 import 'dotenv/config';
-
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+console.log('DB_ADDRESS:', process.env.DB_ADDRESS);
 mongoose.set('strictQuery', true);
 mongoose.connect(DB_ADDRESS);
 
-// Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -23,5 +21,4 @@ app.use(routes);
 app.use(errors());
 app.use(errorHandler);
 
-// eslint-disable-next-line no-console
 app.listen(PORT, () => console.log('ok'));
